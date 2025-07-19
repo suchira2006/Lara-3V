92b2ba782f48d7058a5063ef82d6ef127e24a49e
W.M.Suchira Loshan <138687790+suchira2006@users.noreply.github.com>
2025-07-19 18:26:43 +0530

    Update settings1.js

--- settings.js
+++ settings.js
@@ -5,11 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "icxzFZTI#ajUoNip1y4ByvuGDvSR907GXp4DSgBLf-GDR23h0cn0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
-MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
-DEV: process.env.DEV || "94779062397",
+MODE: process.env.MODE || "private",  //public | private | inbox | group
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94788582834",
+DEV: process.env.DEV || "94788582834",
 LANG: process.env.LANG || "EN", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
